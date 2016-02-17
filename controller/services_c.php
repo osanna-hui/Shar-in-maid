f5341d251513002394fd7353ad1279f866271389
Osanna Hui <osannahui@Osannas-MBP.vc.shawcable.net>
2016-02-16 22:26:52 -0800

    services initial crud

---
+++ controller/services_c.php
@@ -0,0 +1,20 @@
+<?php
+
+include("../model/services_db.php");
+
+if($_POST['method'] == "insert_serv"){
+    insert_services();
+}
+
+if($_POST['method'] == "get_serv"){
+    get_services();
+}
+
+if($_POST['method'] == "update_serv"){
+    update_serv();
+}
+if($_POST['method'] == "delete_serv"){
+    delete_serv();
+}
+
+?>
