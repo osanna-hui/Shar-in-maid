7770bf61652217fdcb07b9e5cda22c858f6bbdbe
Osanna Hui <osannahui@Osannas-MacBook-Pro.local>
2016-02-28 18:17:38 -0800

    get all service post

--- model/servicedb.php
+++ model/servicedb.php
@@ -7,15 +7,18 @@ function insert_service(){
     
     $query = "INSERT INTO services (serv_type, serv_desc, serv_price, serv_area, user_id) VALUES ('".$_POST['type']."', '".$_POST['desc']."', '".$_POST['price']."', '".$_POST['area']."', '".$_POST['user_id']."')";
     $result = $db->query($query);
-    echo json_encode($result->fetchAll());
-    
+    //echo json_encode($result->fetchAll());
+    /*
+    $data = array("status" => "success", "msg" => "service inserted");
+    echo json_encode($data, JSON_FORCE_OBJECT);
+    */
 }
 
 function get_service(){
     //read info for users; get the users by id: when you click on the img, you do a select in the db to grab their info where id is equal to what is clicked on
     global $db;
     
-    $query = "SELECT * FROM users";
+    $query = "SELECT services.serv_id, services.serv_type, services.serv_desc, services.serv_price, services.serv_area, users.username FROM services LEFT JOIN users ON services.user_id = users.user_id";
     $result = $db->query($query);
     
     echo json_encode($result->fetchAll());
@@ -26,7 +29,8 @@ function update_service(){
     //create info for users
     global $db;
     
-    $query = "UPDATE users (username, password, firstname, lastname, email, address, city, profileimg) SET users  ('".$_POST['username']."', '".$_POST['password']."', '".$_POST['firstname']."', '".$_POST['lastname']."', '".$_POST['email']."', '".$_POST['address']."', '".$_POST['city']."', '".$_POST['profileimg']."')";
+    $query = "UPDATE services (serv_type, serv_desc, serv_price, serv_area, user_id) SET services ('".$_POST['type']."', '".$_POST['desc']."', '".$_POST['price']."', '".$_POST['area']."', '".$_POST['user_id']."')";
+
     $result = $db->query($query);
     
     echo json_encode($result->fetchAll());
@@ -35,7 +39,7 @@ function update_service(){
 function delete_service(){
     //delete the usernames
     global $db;
-    $query = "DELETE FROM users WHERE user_id = ".$_POST['user_id']."";
+    $query = "DELETE FROM services WHERE serv_id = ".$_POST['serv_id']."";
     $result = $db->query($query);
     echo json_encode($result->fetchAll());
 }
