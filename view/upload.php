8c649f298c056f1eeb9c64ea8af1a1b1e8992d36
Osanna Hui <osannahui@Osannas-MacBook-Pro.local>
2016-05-20 02:16:43 -0700

    need to sort out upload img

--- view/upload.php
+++ view/upload.php
@@ -3,13 +3,13 @@
     var_dump($_POST);
     var_dump($_FILES);
     
-    if(!is_dir("../uploads/".$_POST['userid'])){
-    	mkdir("../uploads/".$_POST['userid'],0755);
+    if(!is_dir("uploads/".$_POST['userid'])){
+    	mkdir("uploads/".$_POST['userid'],0755);
     }
 
     $filename = $_FILES['images']['name'][0];
     //$filename = substr(md5(rand()), 0, 7);
     
-    move_uploaded_file($_FILES['images']['tmp_name'][0], "../uploads/".$_POST['userid']."/".$filename);
+    move_uploaded_file($_FILES['images']['tmp_name'][0], "uploads/".$_POST['userid']."/".$filename);
 
 ?>
