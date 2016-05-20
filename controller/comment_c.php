41f44a137778e9422e4d3731e8039a1121781416
Anth0z <anthornato@hotmail.com>
2016-05-19 17:48:48 -0700

    insert and view within service_all.html

--- controller/comment_c.php
+++ controller/comment_c.php
@@ -8,8 +8,16 @@ if($_POST['method'] == "insert_comment"){
 if($_POST['method'] == "get_comments"){
     get_comments();
 }
+
 if($_POST['method'] == "get_comment_single_serv"){
     get_comment_single_serv();
 }
 
+if($_POST['method'] == "update_comment"){
+    update_comment();
+}
+
+if($_POST['method'] == "delete_comment"){
+    delete_comment();
+}
 ?>
--- model/commentdb.php
+++ model/commentdb.php
@@ -5,8 +5,9 @@ function insert_comment(){
     //create info for users
     global $db;
     
-    $query = "INSERT INTO comments (text, user_id, serv_id) VALUES ('".$_POST['text']."', '".$_POST['serv_id']."', ".$_POST['user_id'].")";
+    $query = "INSERT INTO comments (text, serv_id, user_id) VALUES ('".$_POST['text']."', '".$_POST['serv_id']."', ".$_POST['user_id'].")";
     $result = $db->query($query);
+    echo $query;
 }
 
 function get_comments(){
@@ -33,15 +34,15 @@ function get_comment_single_serv(){
     
 }
 
-function update_message(){
-    //update info for user
+function update_comment(){
+    //update comment for user
     global $db;
     
-    $query = "UPDATE comments (text, user_id, serv_id) SET comments ('".$_POST['text']."', '".$_POST['serv_id']."', ".$_POST['user_id'].")";
+    $query = "UPDATE comments (text, user_id, serv_id) SET comments ('".$_POST['text']."', '".$_POST['user_id']."', ".$_POST['serv_id'].")";
     $result = $db->query($query);
 }
 
-function delete_message(){
+function delete_comment(){
     //delete info for user
        global $db;
     $query = "DELETE FROM comments WHERE comm_id = ".$_POST['comm_id']."";
